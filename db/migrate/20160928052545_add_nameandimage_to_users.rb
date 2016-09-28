ea933fddfc48e5442c61d843399bbc76169dce6f
kyotoyoda <k.toyoda.cubic@gmail.com>
2016-09-28 16:32:31 +0900

    user nameの初期値を追加

--- db/migrate/20160928052545_add_nameandimage_to_users.rb
+++ db/migrate/20160928052545_add_nameandimage_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameandimageToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :name, :string, null: false, default: "" , unique: true
+    add_column :users, :name, :string, null: false, default: "noname" , unique: true
     add_column :users, :image, :string
   end
 end
