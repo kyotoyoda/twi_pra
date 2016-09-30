0b3ca6c237412dfc5b28af99f248eb28c57ba895
kyotoyoda <k.toyoda.cubic@gmail.com>
2016-09-30 19:27:47 +0900

    Userにマイページへのリンク追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,9 @@ class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
     @tweets = Tweet.all
+    @user = current_user
+    @users = User.all
+
   end
 
   # GET /tweets/1
