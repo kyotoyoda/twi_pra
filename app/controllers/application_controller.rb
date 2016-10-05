f87d8cbe6974ee0023da165d496a0c1fd2ba53fc
kyotoyoda <k.toyoda.cubic@gmail.com>
2016-10-05 11:19:53 +0900

    ユーザーページに画像を投稿できるように（途中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
        end
+
+#    def configure_permitted_parameters
+#      devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+#    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+#  imgae_tag "/user_images/#{user.image}"
+
+#  image_tag "キュービックロゴ.jpg"
 end
