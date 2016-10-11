e53cd3d0a72882e26488e770ec813e9be34c2915
kyotoyoda <k.toyoda.cubic@gmail.com>
2016-10-11 12:52:51 +0900

    followテーブル完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,10 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :likes
 
-  has_many :active_relationships,class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: :active_relationships, source: :following #sつける？
-  has_many :active_relationships,class_name:  "Relationship", foreign_key: "following_id", dependent: :destroy
-  has_many :passive_relationships,class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followers, through: :passive_relationships, source: :follower
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
-
-
+  has_many :active_relationships,foreign_key: :following_id,class_name: Relationship, dependent: :destroy
+  has_many :followings, through: :active_relationships
+  has_many :passive_relationships, foreign_key: :follower_id,class_name: Relationship , dependent: :destroy
+  has_many :followers, through: :passive_relationships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
